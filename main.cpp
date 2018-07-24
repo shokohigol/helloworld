ff1101ce08578028af9dd5f8b280fe4d49a6f9dc
shokohigol <shokohigol@gmail.com>
2018-07-25 00:07:55 +0430

    secend commit

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@ int main(int argc, char *argv[])
 
     std::cout<<"In the name of ALLAH"<<std::endl;
 
+    int n=10;
+    for(int i=1;i<n;i++)
+    {
+        for(int j=n;j>i;j--)
+            std::cout<<" ";
+        std::cout<<"*"<<std::endl;
+    }
+
     std::cin.get();
 
     return a.exec();
