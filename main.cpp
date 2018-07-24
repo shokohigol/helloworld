1febe85c3e1d3afce92c733984c294c719bac734
shokohigol <shokohigol@gmail.com>
2018-07-24 23:49:41 +0430

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    std::cout<<"In the name of ALLAH"<<std::endl;
+
+    std::cin.get();
+
+    return a.exec();
+ }
